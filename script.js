98a27a971d59162e22d6b1e638e2d1ca431a9495
Trang Truong <trangldtruong@gmail.com>
2023-09-11 15:47:20 -0400

    initial draft

--- script.js
+++ script.js
@@ -3,17 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  passwordText = lowerCaseCharacters + upperCaseCharacters + numericCharacters + specialCharacters;
+
+  var passwordLength = window.prompt("Choose between 8 and 128 characters")
+  for (let i=8; i<=128; i++); 
+  passwordLength = Math.floor(Math.random()*passwordLength.length);
+
+  var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  var lowerCase = window.confirm("Do you want to include lowercase characters?");
+  if (lowerCase == true) {
+  password += lowerCase[Math.floor(Math.random() * lowerCaseCharacters.length)];
+  }
+  var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upperCase = window.confirm("Do you want to include uppercase characters?");
+  if (upperCase == true) {
+  password += upperCaseCharacters[Math.floor(Math.random() * upperCaseCharacters.length)];
+  }
+  var numericCharacters = "0123456789";
+  var numeric = window.confirm("Do you want to include numeric characters?");
+  if (numeric == true) {
+  password += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+  }
+  var specialCharacters =  "!@#$%^&*()";
+  var special = window.confirm("Do you want to include special characters?")
+  if (special == true) {
+  password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  }
 
 }
-var lowercase = window.confirm("Do you want a lowercase character?")
+}
 
-var uppercase = window.confirm("Do you want an uppercase character?")
-var numeric = window.confirm("Do you want numeric character?")
-var special = window.confirm("Do you want special character?")
-var length = window.prompt("Choose between 8 and 128 characters")
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
