8c851e9ccef7b136206ab689849e0252a63a57a8
Trang Truong <trangldtruong@gmail.com>
2023-09-11 23:34:18 -0400

    update js code, not sure how to make the loop

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-//function writePassword() {
   function generatePassword() {
     var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
     var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,38 +9,45 @@ var generateBtn = document.querySelector("#generate");
     var passwordText = document.querySelector("#password"); 
     
   
-    var length = window.prompt("Choose between 8 and 128 characters")
-    if (length < 8 || length > 128) {
+    var passwordLength = window.prompt("Choose between 8 and 128 characters")
+    if (passwordLength < 8 || passwordLength > 128) {
     return "Please enter a number between 8 and 128.";
     } else {
-    alert("Your password will have " + length + " characters");
-    }
+    alert("Your password will have " + passwordLength + " characters");
+    };
   
     var lowerCase = window.confirm("Do you want to include lowercase characters?");
-    if (lowerCase == true) {
+    if (lowerCase === true) {
     passwordText += lowerCaseCharacters[Math.floor(Math.random() * lowerCaseCharacters.length)];
     } else 
   
     var upperCase = window.confirm("Do you want to include uppercase characters?");
-    if (upperCase == true) {
+    if (upperCase === true) {
     passwordText += upperCaseCharacters[Math.floor(Math.random() * upperCaseCharacters.length)];
     } else
   
     var numeric = window.confirm("Do you want to include numeric characters?");
-    if (numeric == true) {
+    if (numeric === true) {
     passwordText += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
     } else 
   
     var special = window.confirm("Do you want to include special characters?")
-    if (special == true) {
+    if (special === true) {
     passwordText += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-    } else
-    passwordText = lowerCaseCharacters + upperCaseCharacters + numericCharacters + specialCharacters;
- //make a while loop to iterate the length 
- //for (i=0, i<length, i++)
- passwordText.value = password;
-}
+    } else if (lowerCase === false && upperCase === false && numeric === false && special === false) {
+     return "Please select at least one character type"
+  };
+  //let passwordGenerated = ""
+  //for (let i = 0, i < passwordLength.value, i++) {
+      //passwordGenerated += password[Math.floor(Math.random()*(passwordLength.value))];
+      //passwordText.value = password;}
+  }
+  
+
+  
+
 
 // Add event listener to generate button
+var generateBtn = document.querySelector("#generate")
 generateBtn.addEventListener("click", generatePassword);
-//generatePassword();
+
